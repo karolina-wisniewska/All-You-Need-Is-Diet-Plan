d28e33e5a29b3c64ffb29362df2b2eaf6a37ade1
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-04-11 13:05:19 +0200

    spring security: login and registration form views

--- src/main/java/pl/coderslab/allyouneedisdietplan/controller/LoginController.java
+++ src/main/java/pl/coderslab/allyouneedisdietplan/controller/LoginController.java
@@ -1,4 +1,29 @@
 package pl.coderslab.allyouneedisdietplan.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import pl.coderslab.allyouneedisdietplan.entity.security.User;
+import pl.coderslab.allyouneedisdietplan.service.security.UserService;
+
+@RequiredArgsConstructor
+@Controller
 public class LoginController {
+
+  private final UserService userService;
+  @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+  public String login(){
+    return "/user/login";
+  }
+
+  @RequestMapping(value="/registration", method = RequestMethod.GET)
+  public String registration(Model model){
+    model.addAttribute("user", new User());
+    return "user/registration";
+  }
+
+
 }

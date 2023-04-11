362e66a1c0c99baa65ed293badb98276c152105a
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-04-11 09:03:54 +0200

    first commit

---
+++ src/main/java/pl/coderslab/allyouneedisdietplan/AllYouNeedIsDietPlanApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.allyouneedisdietplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllYouNeedIsDietPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllYouNeedIsDietPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/coderslab/allyouneedisdietplan/AllYouNeedIsDietPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.allyouneedisdietplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllYouNeedIsDietPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

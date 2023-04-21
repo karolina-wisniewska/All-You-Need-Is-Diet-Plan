b9418183d658e4da2a0dc57231b3bd919d4648de
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-04-21 23:39:10 +0200

    Links to change the item added

--- src/main/java/pl/coderslab/allyouneedisdietplan/controller/PlanController.java
+++ src/main/java/pl/coderslab/allyouneedisdietplan/controller/PlanController.java
@@ -4,7 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import pl.coderslab.allyouneedisdietplan.entity.DayName;
 import pl.coderslab.allyouneedisdietplan.entity.DietPlanItem;
@@ -80,14 +81,16 @@ public class PlanController {
     return "plan/show";
   }
 
-  @GetMapping(value = "/user/plan/showDetails")
-  @ResponseBody
-  public String getSingleRecipe() {
-    String url = "https://api.edamam.com/api/recipes/v2/b79327d05b8e5b838ad6cfd9576b30b6?type=public&app_id=40fa347c&app_key=0eb977d62e50265cf4df0451172393a6";
+  @PostMapping(value = "/user/plan/showDetails")
+  public String getSingleRecipe(@RequestParam String url) {
     RestTemplate restTemplate = new RestTemplate();
     RecipeResource recipe = restTemplate.getForObject(url, RecipeResource.class);
-    System.out.println("Recipe: " + recipe);
-    return "Recipe details";
+    String redirectUrl = recipe.getRecipe().getShareAs();
+    return "redirect:" + redirectUrl;
   }
 
+  @PostMapping(value = "/user/plan/reload")
+  public String reloadSingleRecipe() {
+    return "plan/show";
+  }
 }

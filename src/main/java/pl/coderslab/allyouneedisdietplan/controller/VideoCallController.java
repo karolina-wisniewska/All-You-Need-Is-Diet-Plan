8c1d7c410f69c2ce94ca6c22a48f126a5edaef86
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-04-28 13:36:09 +0200

    test and real call

--- src/main/java/pl/coderslab/allyouneedisdietplan/controller/VideoCallController.java
+++ src/main/java/pl/coderslab/allyouneedisdietplan/controller/VideoCallController.java
@@ -2,14 +2,23 @@ package pl.coderslab.allyouneedisdietplan.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @Controller
 public class VideoCallController {
 
-  @GetMapping(value = "/user/call")
-  public String home() {
+  @GetMapping(value = "/user/call/test")
+  public String showTestVideoCall() {
     return "videoCall/index";
+  }
+
+  @GetMapping(value = "/user/call")
+  public String showVideoCall(Model model, Principal principal) {
+    model.addAttribute("userName", principal.getName());
+    return "videoCall/call";
   }
 }
